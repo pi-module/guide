b27147028edd463058060a54f48ffaf772030dfa
voltan <djvoltan@gmail.com>
2014-03-22 10:29:28 +0430

    Add empty table list for start

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1,1 +1,65 @@
- 
+CREATE TABLE `{item}` (
+    `id` int(10) unsigned NOT NULL auto_increment,
+
+    PRIMARY KEY (`id`)
+); 
+
+CREATE TABLE `{category}` (
+    `id` int(10) unsigned NOT NULL auto_increment,
+
+    PRIMARY KEY (`id`)
+); 
+
+CREATE TABLE `{link}` (
+    `id` int(10) unsigned NOT NULL auto_increment,
+
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `{location}` (
+    `id` int(10) unsigned NOT NULL auto_increment,
+
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `{location_category}` (
+    `id` int(10) unsigned NOT NULL auto_increment,
+
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `{attach}` (
+    `id` int(10) unsigned NOT NULL auto_increment,
+
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `{field}` (
+    `id` int(10) unsigned NOT NULL auto_increment,
+
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `{field_data}` (
+    `id` int(10) unsigned NOT NULL auto_increment,
+
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `{service}` (
+    `id` int(10) unsigned NOT NULL auto_increment,
+
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `{review}` (
+    `id` int(10) unsigned NOT NULL auto_increment,
+
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `{blog}` (
+    `id` int(10) unsigned NOT NULL auto_increment,
+
+    PRIMARY KEY (`id`)
+);
