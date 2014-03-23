02214aba197b4d654b59305e22d4ef8978690b3d
voltan <djvoltan@gmail.com>
2014-03-23 11:43:24 +0430

    Add empty controller

---
+++ src/Controller/Admin/ReviewController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Pi Engine (http://pialog.org)
+ *
+ * @link            http://code.pialog.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://pialog.org
+ * @license         http://pialog.org/license.txt New BSD License
+ */
+
+/**
+ * @author Hossein Azizabadi <azizabadi@faragostaresh.com>
+ */
+namespace Module\Guide\Controller\Admin;
+
+use Pi;
+use Pi\Mvc\Controller\ActionController;
+
+class ReviewController extends ActionController
+{
+    public function indexAction()
+    {
+        $test = array(
+        	'Review Controller',
+        );
+        // Set view
+        $this->view()->setTemplate('empty');
+        $this->view()->assign('test', $test);
+    }
+}
