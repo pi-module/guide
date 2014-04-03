64929cd7518f19459387b723b44f461de4b8dc77
voltan <djvoltan@gmail.com>
2014-04-03 11:18:28 +0430

    Add tag and category  controllers

--- src/Controller/Front/CategoryController.php
+++ src/Controller/Front/CategoryController.php
@@ -15,15 +15,50 @@ namespace Module\Guide\Controller\Front;
 use Pi;
 use Pi\Mvc\Controller\ActionController;
 
-class CategoryController extends ActionController
+class CategoryController extends IndexController
 {
-    public function indexAction()
+	public function indexAction()
     {
-        $test = array(
-            'Category Controller',
+        // Get info from url
+        $module = $this->params('module');
+        $slug = $this->params('slug');
+        // Get config
+        $config = Pi::service('registry')->config->read($module);
+        // Get category information from model
+        $category = $this->getModel('category')->find($slug, 'slug');
+        // Check category
+        if (!$category || $category['status'] != 1) {
+            $this->jump(array('', 'module' => $module, 'controller' => 'index'), __('The category not found.'), 'error');
+        }
+        // Set info
+        $where = array('status' => 1, 'category' => $category['id']);
+        // Get item List
+        $itemList = $this->itemList($where);
+        // Set paginator info
+        $template = array(
+            'controller'  => 'category',
+            'slug'        => $slug,
         );
+        // Get paginator
+        $paginator = $this->itemPaginator($template, $where);
+        // category list
+        $categories = Pi::api('category', 'guide')->categoryList($category['id']);
+        // Get special
+        if ($config['view_special']) {
+            $specialList = Pi::api('special', 'guide')->getAll();
+            $this->view()->assign('specialList', $specialList);
+            $this->view()->assign('specialTitle', __('Special items'));
+        }
         // Set view
-        $this->view()->setTemplate('empty');
-        $this->view()->assign('test', $test);
+        $this->view()->headTitle($category['seo_title']);
+        $this->view()->headDescription($category['seo_description'], 'set');
+        $this->view()->headKeywords($category['seo_keywords'], 'set');
+        $this->view()->setTemplate('item_list');
+        $this->view()->assign('itemList', $itemList);
+        $this->view()->assign('itemTitle', sprintf(__('New items on %s'), $category['title']));
+        $this->view()->assign('category', $category);
+        $this->view()->assign('categories', $categories);
+        $this->view()->assign('paginator', $paginator);
+        $this->view()->assign('config', $config);
     }
 }
--- src/Controller/Front/TagController.php
+++ src/Controller/Front/TagController.php
@@ -17,13 +17,86 @@ use Pi\Mvc\Controller\ActionController;
 
 class TagController extends IndexController
 {
-    public function indexAction()
+	public function termAction()
     {
-        $test = array(
-            'Tag Controller',
+        // Get info from url
+        $module = $this->params('module');
+        $slug = $this->params('slug');
+        // Get config
+        $config = Pi::service('registry')->config->read($module);
+        // Check slug
+        if (!isset($slug) || empty($slug)) {
+            $url = array('', 'module' => $module, 'controller' => 'index', 'action' => 'index');
+            $this->jump($url, __('The tag not set.'), 'error');
+        }
+        // Get id from tag module
+        $tagId = array();
+        $tags = Pi::service('tag')->getList($slug, $module);
+        foreach ($tags as $tag) {
+            $tagId[] = $tag['item'];
+        }
+        // Check slug
+        if (empty($tagId)) {
+        	$url = array('', 'module' => $module, 'controller' => 'index', 'action' => 'index');
+            $this->jump($url, __('The tag not found.'), 'error');
+        }
+        // Set info
+        $where = array('status' => 1, 'item' => $tagId);
+        // Get item List
+        $itemList = $this->itemList($where);
+        // Set paginator info
+        $template = array(
+            'controller' => 'tag',
+            'action' => 'term',
+            'slug' => urlencode($slug),
         );
+        // Get paginator
+        $paginator = $this->itemPaginator($template, $where);
+        // Set header and title
+        $title = sprintf(__('All items by %s tag'), $slug);
+        $seoTitle = Pi::api('text', 'guide')->title($title);
+        $seoDescription = Pi::api('text', 'guide')->description($title);
+        $seoKeywords = Pi::api('text', 'guide')->keywords($title);
         // Set view
-        $this->view()->setTemplate('empty');
-        $this->view()->assign('test', $test);
+        $this->view()->headTitle($seoTitle);
+        $this->view()->headDescription($seoDescription, 'set');
+        $this->view()->headKeywords($seoKeywords, 'set');
+        $this->view()->setTemplate('item_list');
+        $this->view()->assign('itemList', $itemList);
+        $this->view()->assign('itemTitle', $title);
+        $this->view()->assign('paginator', $paginator);
+        $this->view()->assign('config', $config);
+    }
+
+    public function listAction()
+    {
+        // Get info from url
+        $module = $this->params('module');
+    	$where = array('module' => $module);
+        $order = array('count DESC', 'id DESC');
+    	$select = Pi::model('stats', 'tag')->select()->where($where)->order($order);
+    	$rowset = Pi::model('stats', 'tag')->selectWith($select);
+    	foreach ($rowset as $row) {
+            $tag = Pi::model('tag', 'tag')->find($row->term, 'term');
+            $tagList[$row->id] = $row->toArray();
+            $tagList[$row->id]['term'] = $tag['term'];
+            $tagList[$row->id]['url'] = $this->url('', array(
+            	'controller' => 'tag', 
+            	'action' => 'term', 
+            	'slug' => urldecode($tag['term'])
+            	));
+        }
+        // Set header and title
+        $title = __('List of all used tags on guide');
+        $seoTitle = Pi::api('text', 'guide')->title($title);
+        $seoDescription = Pi::api('text', 'guide')->description($title);
+        $seoKeywords = Pi::api('text', 'guide')->keywords($title);
+        // Set view
+        $this->view()->headTitle($seoTitle);
+        $this->view()->headDescription($seoDescription, 'set');
+        $this->view()->headKeywords($seoKeywords, 'set');
+        $this->view()->setTemplate('tag_list');
+        $this->view()->assign('title', $title);
+        $this->view()->assign('tagList', $tagList);
     }
 }
