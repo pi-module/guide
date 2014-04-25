1e065189b7065c9798b112a335e4002eae97dd7b
voltan <djvoltan@gmail.com>
2014-04-25 18:31:04 +0430

    Add title for pages

--- config/page.php
+++ config/page.php
@@ -14,50 +14,62 @@ return array(
     // Admin section
     'admin' => array(
         array(
+            'title'         => _a('Item'),
             'controller'    => 'item',
             'permission'    => 'item',
         ),
         array(
+            'title'         => _a('Category'),
             'controller'    => 'category',
             'permission'    => 'category',
         ),
         array(
+            'title'         => _a('Location'),
             'controller'    => 'location',
             'permission'    => 'location',
         ),
         array(
+            'title'         => _a('Service'),
             'controller'    => 'service',
             'permission'    => 'service',
         ),
         array(
+            'title'         => _a('Extra'),
             'controller'    => 'extra',
             'permission'    => 'extra',
         ),
         array(
+            'title'         => _a('Special'),
             'controller'    => 'special',
             'permission'    => 'special',
         ),
         array(
+            'title'         => _a('Review'),
             'controller'    => 'review',
             'permission'    => 'review',
         ),
         array(
+            'title'         => _a('Attach'),
             'controller'    => 'attach',
             'permission'    => 'attach',
         ),
         array(
+            'title'         => _a('Score'),
             'controller'    => 'score',
             'permission'    => 'score',
         ),
         array(
+            'title'         => _a('Package'),
             'controller'    => 'package',
             'permission'    => 'package',
         ),
         array(
+            'title'         => _a('Orders'),
             'controller'    => 'order',
             'permission'    => 'order',
         ),
         array(
+            'title'         => _a('Logs'),
             'controller'    => 'log',
             'permission'    => 'log',
         ),
@@ -65,48 +77,57 @@ return array(
     // Front section
     'front' => array(
         array(
+            'title'         => _a('Index page'),
             'controller'    => 'index',
             'permission'    => 'public',
             'block'         => 1,
         ),
         array(
+            'title'         => _a('Category'),
             'controller'    => 'category',
             'permission'    => 'public',
             'block'         => 1,
         ),
         array(
+            'title'         => _a('Category list'),
             'controller'    => 'category',
             'action'        => 'list',
             'permission'    => 'public',
             'block'         => 1,
         ),
         array(
+            'title'         => _a('Tag'),
             'controller'    => 'tag',
             'permission'    => 'public',
             'block'         => 1,
         ),
         array(
+            'title'         => _a('Tag list'),
             'controller'    => 'tag',
             'action'        => 'list',
             'permission'    => 'public',
             'block'         => 1,
         ),
         array(
+            'title'         => _a('Item'),
             'controller'    => 'item',
             'permission'    => 'public',
             'block'         => 1,
         ),
         array(
+            'title'         => _a('Manage'),
             'controller'    => 'manage',
             'permission'    => 'manage',
             'block'         => 1,
         ),
         array(
+            'title'         => _a('Package'),
             'controller'    => 'package',
             'permission'    => 'public',
             'block'         => 1,
         ),
         array(
+            'title'         => _a('Search'),
             'controller'    => 'search',
             'permission'    => 'public',
             'block'         => 1,
