cf33d4b7a618e6e4dc87b3f4a6c24c5bf44e9b7d
voltan <djvoltan@gmail.com>
2014-05-05 13:37:36 +0430

    improve block template

--- config/module.php
+++ config/module.php
@@ -15,7 +15,7 @@ return array(
     'meta'         => array(
         'title'         => _a('Guide'),
         'description'   => _a('Introducing guides.'),
-        'version'       => '0.1.5',
+        'version'       => '0.1.6',
         'license'       => 'New BSD',
         'logo'          => 'image/logo.png',
         'readme'        => 'docs/readme.txt',
